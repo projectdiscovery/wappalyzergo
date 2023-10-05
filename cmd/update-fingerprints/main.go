60f88ababe96eeba0c27489cd20dc8906f9463e2
BBerastegui <bberastegui@users.noreply.github.com>
2023-10-05 15:03:43 +0200

    Solved typo when referencing to the fingerprints repo.

--- cmd/update-fingerprints/main.go
+++ cmd/update-fingerprints/main.go
@@ -57,7 +57,7 @@ type OutputFingerprint struct {
 	Website     string              `json:"website,omitempty"`
 }
 
-const fingerprintURL = "https://raw.githubusercontent.com/BBerastegui/wappalyzer/master/%s.json"
+const fingerprintURL = "https://raw.githubusercontent.com/BBerastegui/wappalyzer-fingerprints/master/%s.json"
 
 func makeFingerprintURLs() []string {
 	files := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"}
