70c5d1e89a65523d2cc8e892a8e8bab7176eb113
Doğan Can Bakır <dogancanbakir@protonmail.com>
2024-05-14 13:23:25 +0300

    change fingerprint url

--- cmd/update-fingerprints/main.go
+++ cmd/update-fingerprints/main.go
@@ -63,7 +63,7 @@ type OutputFingerprint struct {
 	CPE         string              `json:"cpe,omitempty"`
 }
 
-const fingerprintURL = "https://raw.githubusercontent.com/Lissy93/wapalyzer/master/src/technologies/%s.json"
+const fingerprintURL = "https://raw.githubusercontent.com/enthec/webappanalyzer/main/src/technologies/%s.json"
 
 func makeFingerprintURLs() []string {
 	files := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"}
