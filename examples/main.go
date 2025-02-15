46d530ba28c13b46562dacb04c876ca9c02e3d46
Scott <scottharvey@gmail.com>
2025-02-14 16:41:10 -0800

    change mod

--- examples/main.go
+++ examples/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 
-	wappalyzer "github.com/projectdiscovery/wappalyzergo"
+	wappalyzer "github.com/scottdharvey/wappalyzergo"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/projectdiscovery/wappalyzergo
+module github.com/scottdharvey/wappalyzergo
 
 go 1.21
 
