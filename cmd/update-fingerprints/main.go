63961d31198eb55973dd2fdc4fd5bfd7a9fc1319
Dwi Siswanto <git@dw1.io>
2026-09-03 12:57:44 +0700

    chore(update-fingerprints): satisfy lints
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- cmd/update-fingerprints/main.go
+++ cmd/update-fingerprints/main.go
@@ -136,7 +136,9 @@ func gatherFingerprintsFromURL(URL string, fingerprints *Fingerprints) error {
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
